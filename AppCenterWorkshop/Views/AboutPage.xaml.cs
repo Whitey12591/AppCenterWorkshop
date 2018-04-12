2a89168f2d7eb29ad3cccb9efbed225ef1d5660e
Whitey12591 <danwhite1022@gmail.com>
2018-04-12 10:35:54 -0500

    Initial check-in of module AppCenterWorkshop

---
+++ AppCenterWorkshop/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace AppCenterWorkshop
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
